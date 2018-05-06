ba1f18314bd9722f581bdabd2b3f1060b2f290e9
guido <guido.vespignani@gmail.com>
2018-05-06 13:03:19 -0300

    commit

--- src/app/componentes/piedra-papel-tijera/piedra-papel-tijera.component.ts
+++ src/app/componentes/piedra-papel-tijera/piedra-papel-tijera.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PiedraPapelTijeraComponent implements OnInit {
 
+  eleccionMaquina:number;
   resultado:string;
   constructor() { }
 
@@ -14,45 +15,45 @@ export class PiedraPapelTijeraComponent implements OnInit {
   }
 
   jugar(eleccion:number){
-     let valor = Math.floor(Math.random() * (4 - 1) + 1);
+     this.eleccionMaquina = Math.floor(Math.random() * (4 - 1) + 1);
 
      switch(eleccion){
        case 1:
-        if(valor == 1){
+        if(this.eleccionMaquina == 1){
           this.resultado = 'Empate';
           break;
         }
-        if(valor == 2){
+        if(this.eleccionMaquina == 2){
           this.resultado = 'Gano';
           break;
         }
-        if(valor == 3){
+        if(this.eleccionMaquina == 3){
           this.resultado = 'Perdio';
           break;
         }
         case 2:
-        if(valor == 1){
+        if(this.eleccionMaquina == 1){
           this.resultado = 'Perdio';
           break;
         }
-        if(valor == 2){
+        if(this.eleccionMaquina == 2){
           this.resultado = 'Empate';
           break;
         }
-        if(valor == 3){
+        if(this.eleccionMaquina == 3){
           this.resultado = 'Gano';
           break;
         }
         case 3:
-        if(valor == 1){
+        if(this.eleccionMaquina == 1){
           this.resultado = 'Gano';
           break;
         }
-        if(valor == 2){
+        if(this.eleccionMaquina == 2){
           this.resultado = 'Perdio';
           break;
         }
-        if(valor == 3){
+        if(this.eleccionMaquina == 3){
           this.resultado = 'Empate';
           break;
         }
